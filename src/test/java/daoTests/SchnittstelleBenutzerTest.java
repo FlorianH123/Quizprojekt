4f56699b3d82b959e14bdc1f4db3a92661ca2eb5
FlorianH123 <f.hennrich@gmx.de>
2017-06-28 14:52:00 +0200

    JUnit Tests

--- src/test/java/daoTests/SchnittstelleBenutzerTest.java
+++ src/test/java/daoTests/SchnittstelleBenutzerTest.java
@@ -15,11 +15,14 @@ public class SchnittstelleBenutzerTest {
     private User aUser1 = new User(1        ,"testaccount1@mail.de", "passwort1", "link.link", "testaccount1");
     private User aUser2 = new User(2        ,"testaccount2@mail.de", "passwort2", "link.link", "testaccount2");
     private User aUser3 = new User(3        ,"testaccount3@mail.de", "passwort3", "link.link", "testaccount3");
-    private User aUser4 = new User(0        ,"testaccount4@mail.de", "passwort4", "link.link", "testaccount4");
+    private User aUser4 = new User(99999999 ,"testaccount4@mail.de", "passwort4", "link.link", "testaccount4");
 
     private SchnittstelleBenutzer schnittBenutzer = new SchnittstelleBenutzer();
     private boolean equals;
 
+
+    //Tests für getUserByID Methode
+    //------------------------------------------------------------------------------------------------------------------
     @Test
     public void getUserByIDTest() throws Exception {
         equals = schnittBenutzer.getUserByID(1).getE_mail().equals(aUser1.getE_mail()) &&
@@ -55,7 +58,6 @@ public class SchnittstelleBenutzerTest {
                 schnittBenutzer.getUserByID(3).getAvatar_link().equals(aUser2.getAvatar_link()) &&
                 schnittBenutzer.getUserByID(3).getName().equals(aUser2.getName());
         assertFalse(equals);
-
     }
 
     @Test (expected = DataNotFoundException.class)
@@ -63,6 +65,14 @@ public class SchnittstelleBenutzerTest {
         schnittBenutzer.getUserByID(aUser4.getId());
     }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void getUserByID_with_exceptionTest2() throws Exception {
+        schnittBenutzer.getUserByID(-1);
+    }
+
+
+    // Tests für getPasswordByID Methode
+    //------------------------------------------------------------------------------------------------------------------
     @Test
     public void getPasswordByIDTest() throws Exception {
         equals = schnittBenutzer.getPasswordByID(1).equals(aUser1.getPasswort());
@@ -89,23 +99,75 @@ public class SchnittstelleBenutzerTest {
 
     @Test (expected = DataNotFoundException.class)
     public void getPasswordByIDTest_with_exceptionTest() throws Exception {
-        schnittBenutzer.getPasswordByID(0);
+        schnittBenutzer.getPasswordByID(aUser4.getId());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void getPasswordByIDTest_with_exceptionTest2() throws Exception {
+        schnittBenutzer.getPasswordByID(-1);
+    }
+
+
+    //Tests fuer checkID() Methode
+    //------------------------------------------------------------------------------------------------------------------
+    @Test
+    public void checkIDTest1() throws Exception {
+        equals = schnittBenutzer.checkID(aUser1.getId());
+        assertTrue(equals);
     }
 
     @Test
-    public void checkID() throws Exception {
+    public void checkIDTest2() throws Exception {
+        equals = schnittBenutzer.checkID(aUser2.getId());
+        assertTrue(equals);
     }
 
     @Test
-    public void checkEmail() throws Exception {
+    public void checkIDTest3() throws Exception {
+        equals = schnittBenutzer.checkID(aUser3.getId());
+        assertTrue(equals);
     }
 
     @Test
-    public void addUser() throws Exception {
+    public void checkIDTest4() throws Exception {
+        equals = schnittBenutzer.checkID(aUser4.getId());
+        assertFalse(equals);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void checkIDTest_with_exception() throws Exception {
+        equals = schnittBenutzer.checkID(-1);
+    }
+
+
+    //Tests fuer checkEMail() Methode
+    //------------------------------------------------------------------------------------------------------------------
+    @Test
+    public void checkEmailTest1() throws Exception {
+        equals = schnittBenutzer.checkEmail(aUser1.getE_mail());
+        assertTrue(equals);
     }
 
     @Test
-    public void getNextID() throws Exception {
+    public void checkEmailTest2() throws Exception {
+        equals = schnittBenutzer.checkEmail(aUser2.getE_mail());
+        assertTrue(equals);
     }
 
+    @Test
+    public void checkEmailTest3() throws Exception {
+        equals = schnittBenutzer.checkEmail(aUser3.getE_mail());
+        assertTrue(equals);
+    }
+
+    @Test
+    public void checkEmailTest4() throws Exception {
+        equals = schnittBenutzer.checkEmail(aUser4.getE_mail());
+        assertFalse(equals);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void checkEmailTest_with_exception() throws Exception {
+        equals = schnittBenutzer.checkEmail("");
+    }
 }
