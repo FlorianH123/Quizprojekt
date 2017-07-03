5c2bba01d0845204543f1cea4bae5f04845d10ed
weedric <cedi.schreiner@web.de>
2017-07-03 20:01:55 +0200

    Autorisierung fuer User

--- src/main/java/rest/Filter.java
+++ src/main/java/rest/Filter.java
@@ -1,19 +1,14 @@
 package rest;
 
-import dao.ConnectionKlasse;
 import dao.SchnittstelleBenutzer;
-
-import static constants.DB_Constants.*;
-
+import model.User;
+import security.Verschlüsselung;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -25,46 +20,29 @@ import java.util.StringTokenizer;
 public class Filter implements ContainerRequestFilter{
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        ConnectionKlasse conKlasse = new ConnectionKlasse();
-        Connection connection;
-        String username, password, decodeString, authToken;
+        String email, password, decodeString, authToken;
         StringTokenizer tokenizer;
         byte[] decoded;
-        PreparedStatement statement;
-        ResultSet rs;
         SchnittstelleBenutzer sch = new SchnittstelleBenutzer();
+        User user;
 
         if(requestContext.getUriInfo().getPath().contains("secured")){
+            //Ueberprueft ob im Header Authorization steht
             List<String> authHeader = requestContext.getHeaders().get("Authorization");
             if(authHeader != null && authHeader.size() > 0){
+                //authToken enthaehlt Email und Passwort in Base64
                 authToken = authHeader.get(0);
                 authToken = authToken.replaceFirst("Basic", "");
+                //Umwandlung von Email und Passwort
                 decoded = DatatypeConverter.parseBase64Binary(authToken);
                 decodeString = new String(decoded, "UTF-8");
                 tokenizer = new StringTokenizer(decodeString, ":");
-                username = tokenizer.nextToken();
+                email = tokenizer.nextToken();
                 password = tokenizer.nextToken();
-
-
-                /*
-                try{
-                    connection = conKlasse.getConnection();
-                    statement = connection.prepareStatement(PS_AUTHORIZATION);
-                    statement.setString(1, password);
-                    statement.setString(2, username);
-                    rs = statement.executeQuery();
-
-                    if(rs.next()){
-                        return;
-                    }
-                }catch(SQLException e){
-                    //TODO Logger
-                }
-                /*
-                if ("user".equals(username) && "password".equals(password)) {
+                user = sch.getUserByEmail(email);
+                if(Verschlüsselung.checkPasswordEquals(password,user.getPasswort())){
                     return;
                 }
-                */
 
             }
             Response unauthorizedStatus = Response
