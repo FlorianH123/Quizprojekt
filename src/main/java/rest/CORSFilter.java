67fc142f98a3a0a03b4fc1bee6a75788d9ed6c9c
weedric <cedi.schreiner@web.de>
2017-08-19 16:14:37 +0200

    CORS Implementierung

---
+++ src/main/java/rest/CORSFilter.java
@@ -0,0 +1,29 @@
+package rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * Created by Cedric on 18.08.2017.
+ * FIlter for Preflight
+ */
+@Provider
+public class CORSFilter implements ContainerResponseFilter, ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+
+    }
+}
